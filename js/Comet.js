8eba69269cbd7346b1a0ad920c1cef377fa3c5f8
Luffinage <64473118+Luffinage@users.noreply.github.com>
2022-05-04 19:21:32 +0200

    Added Comet in main page (#6)

---
+++ js/Comet.js
@@ -0,0 +1,394 @@
+const comet = document.getElementById('Comet')
+
+setTimeout(() => {
+    cometPos(comet)
+}, 1)
+
+function cometPos() {
+    comet.style.right = '0px';
+    comet.style.top = '0px'
+}
+setTimeout(() => {
+    cometPos1(comet)
+}, 25)
+
+function cometPos1() {
+    comet.style.right = '50px';
+    comet.style.top = '25px'
+}
+
+setTimeout(() => {
+    cometPos2(comet)
+}, 50)
+
+function cometPos2() {
+    comet.style.right = '100px'
+    comet.style.top = '50px'
+
+}
+
+setTimeout(() => {
+    cometPos3(comet)
+}, 75)
+
+function cometPos3() {
+    comet.style.right = '150px'
+    comet.style.top = '75px'
+
+}
+
+setTimeout(() => {
+    cometPos4(comet)
+}, 100)
+
+function cometPos4() {
+    comet.style.right = '200px'
+    comet.style.top = '100px'
+
+}
+
+setTimeout(() => {
+    cometPos5(comet)
+}, 125)
+
+function cometPos5() {
+    comet.style.right = '250px';
+    comet.style.top = '125px'
+}
+
+setTimeout(() => {
+    cometPos6(comet)
+}, 150)
+
+function cometPos6() {
+    comet.style.right = '300px'
+    comet.style.top = '150px'
+
+}
+
+setTimeout(() => {
+    cometPos7(comet)
+}, 175)
+
+function cometPos7() {
+    comet.style.right = '350px'
+    comet.style.top = '175px'
+
+}
+
+setTimeout(() => {
+    cometPos8(comet)
+}, 200)
+
+function cometPos8() {
+    comet.style.right = '400px'
+    comet.style.top = '200px'
+
+}
+setTimeout(() => {
+    cometPos9(comet)
+}, 225)
+
+function cometPos9() {
+    comet.style.right = '450px';
+    comet.style.top = '225px'
+}
+
+setTimeout(() => {
+    cometPos10(comet)
+}, 250)
+
+function cometPos10() {
+    comet.style.right = '500px'
+    comet.style.top = '250px'
+
+}
+
+setTimeout(() => {
+    cometPos11(comet)
+}, 275)
+
+function cometPos11() {
+    comet.style.right = '550px'
+    comet.style.top = '275px'
+
+}
+
+setTimeout(() => {
+    cometPos12(comet)
+}, 300)
+
+function cometPos12() {
+    comet.style.right = '600px'
+    comet.style.top = '300px'
+
+}
+setTimeout(() => {
+    cometPos13(comet)
+}, 325)
+
+function cometPos13() {
+    comet.style.right = '650px';
+    comet.style.top = '325px'
+}
+
+setTimeout(() => {
+    cometPos14(comet)
+}, 350)
+
+function cometPos14() {
+    comet.style.right = '700px'
+    comet.style.top = '350px'
+
+}
+
+setTimeout(() => {
+    cometPos15(comet)
+}, 375)
+
+function cometPos15() {
+    comet.style.right = '750px'
+    comet.style.top = '375px'
+
+}
+
+setTimeout(() => {
+    cometPos16(comet)
+}, 400)
+
+function cometPos16() {
+    comet.style.right = '800px'
+    comet.style.top = '400px'
+
+}
+
+
+setTimeout(() => {
+    cometPos17(comet)
+}, 425)
+
+function cometPos17() {
+    comet.style.right = '850px'
+    comet.style.top = '425px'
+
+}
+setTimeout(() => {
+    cometPos18(comet)
+}, 450)
+
+function cometPos18() {
+    comet.style.right = '900px';
+    comet.style.top = '450px'
+}
+
+setTimeout(() => {
+    cometPos19(comet)
+}, 475)
+
+function cometPos19() {
+    comet.style.right = '950px'
+    comet.style.top = '475px'
+
+}
+
+setTimeout(() => {
+    cometPos20(comet)
+}, 500)
+
+function cometPos20() {
+    comet.style.right = '1000px'
+    comet.style.top = '500px'
+
+}
+
+setTimeout(() => {
+    cometPos21(comet)
+}, 525)
+
+function cometPos21() {
+    comet.style.right = '1050px'
+    comet.style.top = '525px'
+
+}
+setTimeout(() => {
+    cometPos22(comet)
+}, 550)
+
+function cometPos22() {
+    comet.style.right = '1100px';
+    comet.style.top = '550px'
+}
+
+setTimeout(() => {
+    cometPos23(comet)
+}, 575)
+
+function cometPos23() {
+    comet.style.right = '1150px'
+    comet.style.top = '575px'
+
+}
+
+setTimeout(() => {
+    cometPos24(comet)
+}, 600)
+
+function cometPos24() {
+    comet.style.right = '1200px'
+    comet.style.top = '600px'
+
+}
+
+setTimeout(() => {
+    cometPos25(comet)
+}, 625)
+
+function cometPos25() {
+    comet.style.right = '1250px'
+    comet.style.top = '625px'
+
+}
+
+setTimeout(() => {
+    cometPos26(comet)
+}, 650)
+
+function cometPos26() {
+    comet.style.right = '1300px';
+    comet.style.top = '650px'
+}
+
+setTimeout(() => {
+    cometPos27(comet)
+}, 675)
+
+function cometPos27() {
+    comet.style.right = '1350px'
+    comet.style.top = '675px'
+
+}
+
+setTimeout(() => {
+    cometPos28(comet)
+}, 700)
+
+function cometPos28() {
+    comet.style.right = '1400px'
+    comet.style.top = '700px'
+
+}
+setTimeout(() => {
+    cometPos29(comet)
+}, 725)
+
+function cometPos29() {
+    comet.style.right = '1450px'
+    comet.style.top = '725px'
+
+}
+
+setTimeout(() => {
+    cometPos30(comet)
+}, 750)
+
+function cometPos30() {
+    comet.style.right = '1500px';
+    comet.style.top = '750px'
+}
+
+setTimeout(() => {
+    cometPos31(comet)
+}, 775)
+
+function cometPos31() {
+    comet.style.right = '1550px'
+    comet.style.top = '775px'
+
+}
+
+setTimeout(() => {
+    cometPos32(comet)
+}, 800)
+
+function cometPos32() {
+    comet.style.right = '1600px'
+    comet.style.top = '800px'
+
+}
+
+setTimeout(() => {
+    cometPos33(comet)
+}, 825)
+
+function cometPos33() {
+    comet.style.right = '1650px'
+    comet.style.top = '825px'
+
+}
+
+setTimeout(() => {
+    cometPos34(comet)
+}, 850)
+
+function cometPos34() {
+    comet.style.right = '1700px';
+    comet.style.top = '850px'
+}
+
+setTimeout(() => {
+    cometPos35(comet)
+}, 875)
+
+function cometPos35() {
+    comet.style.right = '1750px'
+    comet.style.top = '875px'
+
+}
+
+setTimeout(() => {
+    cometPos36(comet)
+}, 900)
+
+function cometPos36() {
+    comet.style.right = '1800px'
+    comet.style.top = '900px'
+
+}
+setTimeout(() => {
+    cometPos37(comet)
+}, 925)
+
+function cometPos37() {
+    comet.style.right = '1850px'
+    comet.style.top = '925px'
+
+}
+
+setTimeout(() => {
+    cometPos38(comet)
+}, 950)
+
+function cometPos38() {
+    comet.style.right = '1900px';
+    comet.style.top = '950px'
+}
+
+setTimeout(() => {
+    cometPos39(comet)
+}, 975)
+
+function cometPos39() {
+    comet.style.right = '1950px'
+    comet.style.top = '975px'
+
+}
+
+setTimeout(() => {
+    cometPos40(comet)
+}, 1000)
+
+function cometPos40() {
+    comet.style.right = '2000px'
+    comet.style.top = '1000px'
+
+}
